3116cd5e43459556f6e8556a598d1178970699d3
jztimm <jztimm@gmail.com>
2020-11-15 14:00:44 -0500

    Get header and home comp. Add logo to header

---
+++ src/Components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../Styles/Header.css'
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header_icon"
+        src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+        alt="Vacafy Logo"
+      />
+
+      <div className="header_search">
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
