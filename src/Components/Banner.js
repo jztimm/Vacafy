3c991f9513405c3d45b205994af23ee88b6dae5e
jztimm <jztimm@gmail.com>
2020-11-15 20:14:26 -0500

    Setup Date Picker

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../Styles/Banner.css'
 import {Button} from '@material-ui/core'
+import Search from './Search'
 
 export default function Banner() {
+
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <div className="banner">
       <div className="banner_search">
-        <Button 
+        {showSearch && <Search />}
+        <Button onClick={() => {
+          setShowSearch(!showSearch)
+        }}
           className="banner_searchButton"
-          variant='outlined'>Search Dates
+          variant='outlined'>
+            {showSearch ? "Hide" :
+            "Search Dates"}
         </Button>
       </div>
-      
+
       <div className="banner_info">
+        {/* <h1>Go Beyond</h1> */}
         <h1>Get out and stretch your imagination</h1>
         <h5>
           Plan a different kind of getaway to uncover the gems near you.
